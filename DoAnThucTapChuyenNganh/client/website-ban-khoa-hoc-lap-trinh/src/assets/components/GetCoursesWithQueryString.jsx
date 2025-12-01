8f6e079fdb56585392ea14dc335defb630276823
chungdt051204 <email_github_ca_bn@gmail.com>
2025-12-01 21:13:43 +0700

    update

--- DoAnThucTapChuyenNganh/client/website-ban-khoa-hoc-lap-trinh/src/assets/components/GetCoursesWithQueryString.jsx
+++ DoAnThucTapChuyenNganh/client/website-ban-khoa-hoc-lap-trinh/src/assets/components/GetCoursesWithQueryString.jsx
@@ -9,6 +9,7 @@ export default function GetCoursesWithQueryString({ data }) {
               <div className="coursesWithQueryString-item" key={index}>
                 <img src={value.image} alt="" width={150} height={200} />
                 <p>{value.title}</p>
+                {value.price > 0 ? <p>{value.price}</p> : <p>Miễn phí</p>}
               </div>
             );
           })
